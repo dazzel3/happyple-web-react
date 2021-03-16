7a16fc369304e7001ca333d68b8a96f553de6148
dazzel3 <aakk9350@naver.com>
2021-03-17 00:14:22 +0900

    header logo 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Header.css';
+import logo from '../assets/logo_white.png'
 
 function Header() {
 
@@ -27,15 +28,17 @@ function Header() {
     // });
 
     return (
-        <div className="Wrapper">
-            <nav className = "Menu">
-                <Link className="MenuItem is-active" active-color='orange' to = "/">Home</Link>
-                <Link className="MenuItem" active-color='red' to = "/about">About</Link>
-                <Link className="MenuItem" active-color='green' to = "/exhibition">Exhibition</Link>
-                <Link className="MenuItem" active-color='blue' to = "/shop">Shop</Link>
-                <span class="MenuIndicator"></span>
-            </nav>
+        <div className="all">
+            <img className="logo" src={logo} />    
+                <nav className = "Menu">
+                    <Link className="MenuItem is-active" active-color='orange' to = "/">Home</Link>
+                    <Link className="MenuItem" active-color='red' to = "/about">About</Link>
+                    <Link className="MenuItem" active-color='green' to = "/exhibition">Exhibition</Link>
+                    <Link className="MenuItem" active-color='blue' to = "/shop">Shop</Link>
+                    <span class="MenuIndicator"></span>
+                </nav>
         </div>
+        
     )
 }
 
