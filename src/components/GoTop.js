e7f46c70d9a9831b469ce6468d8b5f3835301435
dazzel3 <aakk9350@naver.com>
2021-03-21 03:03:02 +0900

    goTop button 생성

---
+++ src/components/GoTop.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import '../css/GoTop.css';
+import gotop from '../assets/gotop.png'
+
+function GoTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 270) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    <div className="scroll-to-top">
+      {isVisible && 
+        <div onClick={scrollToTop}>
+          <img src={gotop}/>
+        </div>}
+    </div>
+  );
+}
+
+export default GoTop;
