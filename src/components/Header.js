5ffc4441aabf14179c348fd0f34966a87d14c75c
dazzel3 <aakk9350@naver.com>
2021-04-04 23:40:41 +0900

    Header 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,11 +9,11 @@ function Header() {
         <div className="all">
             <img className="logo" src={logo} />    
                 <nav className = "Menu">
-                    <Link className="MenuItem is-active" active-color='orange' to = "/">Home</Link>
-                    <Link className="MenuItem" active-color='red' to = "/about">About</Link>
-                    <Link className="MenuItem" active-color='green' to = "/exhibition">Exhibition</Link>
-                    <Link className="MenuItem" active-color='blue' to = "/shop">Shop</Link>
-                    <Link className="MenuItem" active-color='blue' to = "/FAQ">FAQ</Link>
+                    <Link className="MenuItem" to = "/">Home</Link>
+                    <Link className="MenuItem" to = "/about">About</Link>
+                    <Link className="MenuItem" to = "/exhibition">Exhibition</Link>
+                    <Link className="MenuItem" to = "/shop">Shop</Link>
+                    <Link className="MenuItem" to = "/FAQ">FAQ</Link>
                     <span class="MenuIndicator"></span>
                 </nav>
                 <button className="request-btn">의뢰하기</button>    
