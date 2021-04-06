dab501fa2ba15c6b03d5f294c84ace682bb9143b
dazzel3 <aakk9350@naver.com>
2021-04-07 00:30:09 +0900

    exhibition 디자인 수정

--- src/components/Exhibition.js
+++ src/components/Exhibition.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
 import '../css/Exhibition.css';
 import AOS from 'aos';
@@ -21,26 +22,27 @@ function Exhibition() {
                 <div className="ex-dropdown">
                     <button className="ex-dropbtn"> 최신순</button>
                     <div className="ex-dropdown-content">
-                        <a href="#"> 최신순 </a>
-                        <a href="#"> 인기순 </a>
-                        <a href="#"> 조회순 </a>
+                        <a href="#latest"> 최신순 </a>
+                        <a href="#popularity"> 인기순 </a>
+                        <a href="#inquiry"> 조회순 </a>
                     </div>
                 </div>
                 <div className="wrapper">
                     <div class="card">
                         <img className="exhibition-lang" src={lang} />
                         <div class="info">
-                            <h1>Back slip</h1>
+                            <h1>Time Slip</h1>
                             <p>I want to go back before corona19 virus. i will go jeju-island at july.</p>
-                            <button>OPEN</button>
+                            <Link
+                                to = "/exhibition-timeslip"><button id="open-btn">OPEN</button></Link>
                         </div>
                     </div>
-                    <div class="card">
+                    <div class="card close">
                         <img className="exhibition-comming_soon" src={comming_soon} />
                         <div class="info">
                         <h1>Road</h1>
                         <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
-                        <button>OPEN</button>
+                        <button id="close-btn">CLOSE</button>
                         </div>
                     </div>
                     <div class="card">
@@ -48,7 +50,7 @@ function Exhibition() {
                         <div class="info">
                             <h1>Protester</h1>
                             <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
-                            <button>OPEN</button>
+                            <button id="close-btn">CLOSE</button>
                         </div>
                     </div>
                 </div>
