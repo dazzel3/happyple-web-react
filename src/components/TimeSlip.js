eb1bf260aebf4e80ffe8f55ced3f22ecf5cd1df7
se-ryoung <qm314@naver.com>
2021-05-16 00:02:59 +0900

    05.16 수정

--- src/components/TimeSlip.js
+++ src/components/TimeSlip.js
@@ -264,7 +264,7 @@ function TimeSlip() {
                         <div
                             style={{ zIndex: 100, position: "relative" }}
                             onClick={soundUp}
-                            className="volume_pluss"
+                            className="volume_plus"
                         >
                             +
                         </div>
